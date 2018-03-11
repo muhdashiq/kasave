f367476c1c397de979adfffaffe7b554d410a881
Muhammed Ashiq <ashiqm@amazon.com>
2018-03-11 13:14:47 +0530

    Photo upload form and backend

--- routes/membership.js
+++ routes/membership.js
@@ -2,17 +2,54 @@ var express = require('express');
 var router = express.Router();
 var members = require('../members/members');
 var recaptcha = require('../security/recaptcha');
+var multer = require('multer');
 const uuidv1 = require('uuid/v1');
 
 const utils = require('../utils/savefile')
 
-/* GET users listing. */
+var Storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "./data/images");
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+
+var upload = multer({
+     storage: Storage
+ }).array("imgUploader", 3);
+
+/* Image upalod*/
+ router.get('/photo', recaptcha.middleware.render, function(req, res) {
+   res.render('photoUpload', { title: 'Kasave', captcha: res.recaptcha });
+});
+
+/* Image upalod*/
+router.post('/photo', function(req, res) {
+  console.log(req);
+   upload(req, res, function(err) {
+       if (err) {
+           return res.end("Something went wrong!");
+       }
+       return res.end("File uploaded sucessfully!.");
+   });
+});
+
+/* GET for the membership registration form*/
+router.get('/', recaptcha.middleware.render, function(req, res, next) {
+  res.render('membership_registration', { title: 'Kasave', captcha: res.recaptcha });
+});
+
+/* POST the membership form. */
 router.post('/', recaptcha.middleware.verify, function(req, res, next) {
+  res.render('messages', { title: 'Error!!!', message: JSON.stringify(req.body) });
   const id = uuidv1();
   if (!req.recaptcha.error) {
     const {file, name, address, address2, gender, email, panjayath, ward, houseNo, mobile, guardian, education, bloodGroup, otherOrganisation}  = req.body;
     //Saving photo /data/images
-    const targeImageLocation = utils.saveFileToLocation(req.files.file,path.resolve(`./data/${id}.png`))
+    //const targeImageLocation = utils.saveFileToLocation(req.files.file,path.resolve(`./data/${id}.png`))
     if(members.add({id, name, address, address2, gender, email, panjayath, ward, houseNo, mobile, guardian, education, bloodGroup, otherOrganisation})){
       res.render('messages', { title: 'Registration completed!!!', message: `Membership request successfully added to our system, our coordinator will get in touch with you earliest as possible. Your unique registration id is ${id}, we highly recomend you to take a screenshot of this for your quick reference(s).`});
     } else {
@@ -22,10 +59,6 @@ router.post('/', recaptcha.middleware.verify, function(req, res, next) {
   else {
     res.render('messages', { title: 'Error!!!', message: 'Failed to verify the captcha. Please try again.' });
   }
-});
-
-router.get('/', recaptcha.middleware.render, function(req, res, next) {
-  res.render('membership_registration', { title: 'Kasave', captcha: res.recaptcha });
 });
 
 /* GET users listing. */
